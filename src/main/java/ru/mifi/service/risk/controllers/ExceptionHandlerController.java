6fe341428db39594bcfeca51c15b97c517ee5346
DenRUS <denrus1995@gmail.com>
2018-09-29 21:59:10 +0300

    1) Swagger 2) Правки конфигов 3) Тестовый контроллер

---
+++ src/main/java/ru/mifi/service/risk/controllers/ExceptionHandlerController.java
@@ -0,0 +1,21 @@
+package ru.mifi.service.risk.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import ru.mifi.service.risk.exception.RestException;
+
+@Controller
+public class ExceptionHandlerController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ExceptionHandlerController.class);
+
+    @ExceptionHandler(RestException.class)
+    public @ResponseBody
+    String handleException(RestException e) {
+        LOG.error("Ошибка: " + e.getMessage(), e);
+        return "Ошибка: " + e.getMessage();
+    }
+}
