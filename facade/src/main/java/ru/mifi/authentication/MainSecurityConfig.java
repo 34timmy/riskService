85beea09b5250c233c41031cf786915bca49664f
DenRUS <denrus95@mail.ru>
2018-11-20 02:18:58 +0300

    Выключил авторизацию для local

--- facade/src/main/java/ru/mifi/authentication/MainSecurityConfig.java
+++ facade/src/main/java/ru/mifi/authentication/MainSecurityConfig.java
@@ -1,19 +1,20 @@
 package ru.mifi.authentication;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.access.AccessDeniedHandler;
 
 @Configuration
 @EnableWebSecurity
 public class MainSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${local.auth}")
+    private Boolean localAuth;
 
 
     @Bean
@@ -23,19 +24,23 @@ public class MainSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/", "/js/**", "/css/**").permitAll()
-                .antMatchers("/admin/**").hasAnyRole("ADMIN")
-                .antMatchers("/user/**").hasAnyRole("USER")
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
+        if (localAuth) {
+            http.csrf().disable().authorizeRequests().antMatchers("/**").permitAll();
+        } else {
+            http.csrf().disable()
+                    .authorizeRequests()
+                    .antMatchers("/", "/js/**", "/css/**").permitAll()
+                    .antMatchers("/admin/**").hasAnyRole("ADMIN")
+                    .antMatchers("/user/**").hasAnyRole("USER")
+                    .anyRequest().authenticated()
+                    .and()
+                    .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
+                    .and()
+                    .logout()
+                    .permitAll();
+        }
 //                .and()
 //                .exceptionHandling().accessDeniedHandler(accessDeniedHandler);
     }
--- facade/src/main/java/ru/mifi/authentication/model/AuthorizedUser.java
+++ facade/src/main/java/ru/mifi/authentication/model/AuthorizedUser.java
@@ -2,7 +2,6 @@ package ru.mifi.authentication.model;
 
 
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
